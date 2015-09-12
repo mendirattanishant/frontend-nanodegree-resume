72df15c822a50780c9527e3416e230f93ab36507
Nishant Mendiratta <nmendiratta@Nishants-MacBook-Pro.local>
2015-09-12 13:44:28 -0700

    Added Header to Resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var FName = HTMLheaderName.replace("%data%", "Nishant Mendiratta");
+ $("#header").append(FName);
+ var FRole = HTMLheaderRole.replace("%data%", "Full Stack Developer");
+ $("#header").append(FRole);
